4aff49993bef40fe0df28c789841f8167bc52e6c
Abdul Samad <abdulsamad.official@gmail.com>
2019-03-18 16:26:18 +0500

    CPP and Header Files declared

---
+++ src/ArduinoML.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// Our own Header File
+#include <ArduinoML.h>
+
+ArduinoML::ArduinoML(){
+
+}
+
+ArduinoML::begin(){
+    
+}
+
+ArduinoML::train(){
+
+}
+
+ArduinoML::predict(){
+
+}
+
+ArduinoML::evaluate(){
+
+}
---
+++ src/ArduinoML.h
@@ -0,0 +1,35 @@
+/***************************************************
+
+Copyrights 2019
+Muhammad Hashir Bin Khalid (github: MuhammadHashirBinKhalid)
+Abdul Samad (github: codeWhim)
+
+MIT License
+
+***************************************************/
+
+#ifndef ARDUINOML_H_
+#define ARDUINOML_H_
+
+#include <stdint.h>
+
+// Directives
+
+// Variables
+
+// Constants
+
+// Classes
+class ArduinoML {
+
+public:
+    explicit ArduinoML();
+    void begin();
+
+protected:
+
+private:
+
+};
+
+#endif  // ARDUINOML_H_
