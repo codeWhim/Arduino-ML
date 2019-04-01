3469d183e60a6c9108c117a6982765297fc00eb4
Muhammad Hashir Bin Khalid - <mohammadhashirbinkhalid@gmail.com>
2019-04-01 12:46:34 +0500

    Make things simple

--- src/ArduinoML.cpp
+++ src/ArduinoML.cpp
@@ -10,54 +10,79 @@ ArduinoML::ArduinoML(){
  _spiffsActive=false;
 }
 
-bool ArduinoML::initDataFile(int dataset_number){
-     if (SPIFFS.begin()) {
-     _spiffsActive = true; 
-    _tempstring= FILENAME + String(dataset_number);
-      f = SPIFFS.open(_tempstring, "w");
-      f==1? f.close: return 0;
-      return 1;
-  } else {
-    return 0;
-  }
+
+bool ArduinoML::initDataFile(String filename){
+    f=SPIFFS.open(filename,"w");
+    if(!f) return 0;
+    f.close(); return 1;
 }
 
-bool ArduinoML::removeDataFile(int dataset_number){
-    _tempstring=FILENAME + String(dataset_number);
-    return SPIFFS.remove(_tempstring);
-}
+bool ArduinoML::readExample()
 
-bool ArduinoML::addData(int dataset_number, float value, int axis){    //1=r,2=c,3=pages
-    _tempstring = FILENAME + String(dataset_number);
-    f = SPIFFS.open(_tempstring, "a");
-    switch (axis)
-    {
-    case 1:
-        f.print(',');
-        f.print(String(value));
-        break;
-    case 2:
-        f.print('\n');
-        f.print(String(value));
-        break;
-       }
- f==1? f.close: return 0;    
-}
+// bool ArduinoML::initDataFile(int dataset_number){
+//      if (SPIFFS.begin()) {
+//      _spiffsActive = true; 
+//     _tempstring= FILENAME + String(dataset_number);
+//       f = SPIFFS.open(_tempstring, "w");
+//       f==1? f.close(): return 0;
+//       return 1;
+//   } else {
+//     return 0;
+//   }
+// }
 
-bool ArduinoML::add(int dataset_number, float value){
-   return addData(dataset_number, value,1);
-}
+// bool ArduinoML::removeDataFile(int dataset_number){
+//     _tempstring=FILENAME + String(dataset_number);
+//     return SPIFFS.remove(_tempstring);
+// }
 
-bool ArduinoML::add(int dataset_number, float *value, int valuesize){
-addData(dataset_number,*value,2)
- for(int t=1;t<valuesize;t++){
-     addData(dataset_number,*(value+t),1);
-}
-}
+// bool ArduinoML::addData(int dataset_number, float value, int axis){    //1=r,2=c,3=pages
+//     _tempstring = FILENAME + String(dataset_number);
+//     f = SPIFFS.open(_tempstring, "a");
+//     if(f){
+//     switch (axis)
+//     {
+//     case 1:
+//         f.print(',');
+//         f.print(String(value));
+//         break;
+//     case 2:
+//         f.print('\n');
+//         f.print(String(value));
+//         break;
+//        }
+//        f.close(); return 1;
+//        }
+//        else return 0;
+// }
 
-ArduinoML::setHyperParameters(alpha,optimizer,loss_function){
-    _alpha=alpha;
-}
+// bool ArduinoML::add(int dataset_number, float value){
+//    return addData(dataset_number, value,1);
+// }
+
+// bool ArduinoML::add(int dataset_number, float *value, int valuesize){
+// addData(dataset_number,*value,2)
+//  for(int t=1;t<valuesize;t++){
+//      addData(dataset_number,*(value+t),1);
+// }
+// }
+
+// ArduinoML::setHyperParameters(alpha,optimizer,loss_function){
+//     _alpha=alpha;
+// }
+
+// String ArduinoML::readExample(int dataset_number,int number_of_example){
+//     f = SPIFFS.open(_tempstring, "r");
+//     if(!f) return 0;
+//     for(:,number_of_example>0;number_of_example--)
+//     {f.readStringUntil('\n');}
+//     String mystr=f.readStringUntil('\n');
+//     f.close();
+//     return mystr;
+// }
+
+// void ArduinoML::setDatasetNumber(int dataset_number){ _dataset_number=dataset_number;}
+// int ArduinoML::readNextValue(int dataset_number,)
 
 ArduinoML::train(){
 
